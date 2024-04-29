38bb0a113181ec4b22b7c8382932dc685285b43f
RathiAbhi <abhijeetrathi22@gmail.com>
2024-04-29 13:06:36 +0530

    sharing a resource using synchronized

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,30 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SharedResource sharedBuffer = new SharedResource(3);
+
+        // producer thread using a lambda expression
+        Thread producerThread = new Thread(() ->{
+            try {
+                for (int i=1; i<=6; i++){
+                    sharedBuffer.produce(i);
+                }
+            } catch (Exception e){
+
+            }
+        });
+
+        Thread consumerThread = new Thread(()-> {
+            try{
+                for (int i=1; i<=6; i++){
+                    sharedBuffer.consume();
+                }
+            } catch (Exception e){
+
+            }
+        });
+
+        producerThread.start();
+        consumerThread.start();
     }
 }
