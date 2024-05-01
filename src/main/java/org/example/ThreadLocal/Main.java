0ade456ebbfb9390b8c883ac0f66fd1cf74c71c3
RathiAbhi <abhijeetrathi22@gmail.com>
2024-05-01 13:35:57 +0530

    Thread Local variable

---
+++ src/main/java/org/example/ThreadLocal/Main.java
@@ -0,0 +1,18 @@
+package org.example.ThreadLocal;
+
+public class Main {
+
+    public static void main(String[] args){
+        ThreadLocal<String> threadLocalObj = new ThreadLocal<>();
+        threadLocalObj.set(Thread.currentThread().getName());
+
+        Thread t1 = new Thread(()->{
+            threadLocalObj.set(Thread.currentThread().getName());
+            System.out.println(threadLocalObj.get());
+        });
+
+        t1.start();
+
+        System.out.println(threadLocalObj.get());
+    }
+}
