bc2c81c365088bcc59d2694afb1f5738ef469170
RathiAbhi <abhijeetrathi22@gmail.com>
2024-05-01 13:07:31 +0530

    callable schedule

--- src/main/java/org/example/ExecutorUtilityExample/ScheduledThreadPool.java
+++ src/main/java/org/example/ExecutorUtilityExample/ScheduledThreadPool.java
@@ -1,6 +1,7 @@
 package org.example.ExecutorUtilityExample;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -9,10 +10,15 @@ public class ScheduledThreadPool {
     public static void main(String[] args){
         ScheduledExecutorService poolObj = Executors.newScheduledThreadPool(5);
 
-        poolObj.schedule(()->{
-            System.out.println("hello");
+        Future<String> futureObj =  poolObj.schedule(()->{
+            return "hello";
         }, 5, TimeUnit.SECONDS);
 
+        try {
+            System.out.println(futureObj.get());
+        } catch (Exception e){
+
+        }
         poolObj.shutdown();
     }
 }
